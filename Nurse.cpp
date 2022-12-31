0ccd49f689850af5c42ca249fee13fcc51505e5e
manhta <tamanh7a2@gmail.com>
2022-12-31 16:58:47 +0700

    Sua xuat txt

--- Nurse.cpp
+++ Nurse.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 const int salaryNs = 200000; // tien luong 1 ngay cua y ta
-const int salaryDuty = 500000; // tien luong 1 dem truc
+const int salaryShift = 500000; // tien luong 1 dem truc
 class Nurse : public Person {
     private:
         int dayNs; // so ngay lam viec cua y ta
@@ -15,7 +15,7 @@ class Nurse : public Person {
             this->shift = shift;
         }
         long salary() {
-            return dayNs*salaryNs+shift*salaryDuty;
+            return dayNs*salaryNs+shift*salaryShift;
         }
         void display() {
             Person::display();
